066ab268c7e9bc77d3429da5c50d8b7286b2f70c
udaykiran199715 <udaykiran199715@gmail.com>
2020-07-22 01:33:31 +0530

    top locations

---
+++ tripping/src/Components/TopLocations.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function TopLocations() {
+    return (
+        <div className='container mt-5'>
+                <h2 className='font-weight-bold margin-left'>Top Locations</h2>
+            <div className='row text-secondary mt-3 mx-4'>
+                 <div className='col-3 '>
+                     <h6>Asheville</h6>
+                     <h6>BlueRidge</h6>
+                     <h6>Coaca Beach</h6>
+                     <h6>Fort Myers Beach</h6>
+                     <h6>Key West</h6>
+                     <h6>Los Angeles</h6>
+                     <h6>NashVille</h6>
+                     <h6>Orange Beach</h6>
+                     <h6>Pigeon Forge</h6>
+                     <h6>Seattle</h6>
+                 </div>
+                 <div className='col-3'>
+                     <h6>Atlanta</h6>
+                     <h6>Boston</h6>
+                     <h6>Denver</h6>
+                     <h6>Gatlinburg</h6>
+                     <h6>Kissimmee</h6>
+                     <h6>Miami Beach</h6>
+                     <h6>New Orleans</h6>
+                     <h6>Orlando</h6>
+                     <h6>San Antonio</h6>
+                     <h6>Surfside Beach</h6>
+                 </div>
+                 <div className='col-3'>
+                     <h6>Austin</h6>
+                     <h6>Chicago</h6>
+                     <h6>Destin</h6>
+                     <h6>Gulf Shores</h6>
+                     <h6>Lake Tahoe</h6>
+                     <h6>Montauk</h6>
+                     <h6>New Smyrna Beach</h6>
+                     <h6>Outer Banks</h6>
+                     <h6>Sen Diego</h6>
+                     <h6>Tampa</h6>
+                 </div>
+                 <div className='col-3'>
+                     <h6>Big Bear Lake</h6>
+                     <h6>ClearWater Beach </h6>
+                     <h6>Ford Lauderable</h6>
+                     <h6>Hampton Beach</h6>
+                     <h6>Las Vegas</h6>
+                     <h6>Myrtle Beach</h6>
+                     <h6>New York City</h6>
+                     <h6>Panama City Beach</h6>
+                     <h6>San Francisco</h6>
+                     <h6>Virginia Beach</h6>
+                 </div>
+            </div>
+            
+        </div>
+    )
+}
