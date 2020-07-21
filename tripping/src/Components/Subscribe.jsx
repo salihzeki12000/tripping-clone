ee7f86679748cc91f21c33a96120dbab1411d42a
udaykiran199715 <udaykiran199715@gmail.com>
2020-07-21 23:10:15 +0530

    updated Routes

---
+++ tripping/src/Components/Subscribe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Subscribe() {
+    return (
+        <div className='row container'>
+            <div className='col-6'>
+               <h2></h2>
+            </div>
+            <div className='col-6'>
+
+            </div>
+        </div>
+    )
+}
