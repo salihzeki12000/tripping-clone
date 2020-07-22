2083b3735b0afe6d5dd0189e00b75451e4c4c36a
udaykiran199715 <udaykiran199715@gmail.com>
2020-07-22 20:43:31 +0530

    imported store and provider

--- tripping/src/index.js
+++ tripping/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './Redux/Store'
 
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
