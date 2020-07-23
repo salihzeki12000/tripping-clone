836d5820ba0e5c3cf1034906d9c9539c5c006e01
gangadhar222 <gangadhar.eee@hotmail.com>
2020-07-23 19:00:47 +0530

    saving data

--- tripping/src/App.js
+++ tripping/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar';
+import CardComponent from './Components/CardComponent';
+import CarouselCard from './Components/CarouselCard';
+import FilterNavBar from './Components/FilterComponents/FilterNavBar';
+import Amenities from './Components/FilterComponents/Amenites'
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
+      {/* <SearchBar /> */}
+      {/* <CardComponent /> */}
+      {/* <CarouselCard /> */}
+      <FilterNavBar />
+      <Amenities />
     </div>
   );
 }
