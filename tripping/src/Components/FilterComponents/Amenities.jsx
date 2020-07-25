d8ed3c59641a9d5deedc775de4df706c5b951335
udaykiran199715 <udaykiran199715@gmail.com>
2020-07-26 03:04:46 +0530

    Added Amenities

--- tripping/src/Components/FilterComponents/Amenities.jsx
+++ tripping/src/Components/FilterComponents/Amenities.jsx
@@ -1,52 +1,62 @@
 import React from 'react'
+import './Amenities.css'
 
 export default function Amenities() {
-    let amenites = ['Pet allowed', 'Kitchen', 'TV', 'DishWasher', 'Microwave', 'Jacuzzi', 'No pets', 'Wheelchair access',
-        'Pool', 'Air conditioning', 'Balcony/Patio', 'Grill', 'No smoking', 'Sauna', 'Fishing', 'Fenced', 'Internet',
-        'Washer', 'Parking', 'Yard', 'Fireplace', 'Smoking allowed', 'Crib', 'Detached'
+    let amenites = [{ logo: "fa fa-wifi", name: "Internet", id: "internet" }, { logo: "fa fa-cutlery", name: "Kitchen", id: "kitchen" },
+    { logo: "fas fa-swimming-pool", name: "Pool", id: "pool" }, { logo: "fa fa-television", name: "TV", id: "tv" },
+    { logo: "far fa-snowflake", name: "Air Conditioning", id: "air_conditioning" }, { logo: "fas fa-fan", name: "Washer", id: "washer" },
+    { logo: "fas fa-door-closed", name: "Balcony/Patio", id: "balacony" }, { logo: "fas fa-dog", name: "Pet allowed", id: "pet_allowed" },
+    { logo: "fas fa-fire", name: "Fireplace", id: "fireplace" }, { logo: "fa-fa-glass", name: "Dishwasher", id: "dis_watcher" }, { logo: "fas fa-paw", name: "No pets", id: "no_pets" },
+    { logo: "fas fa-smoking", name: "Smoking", id: "smoking" }, { logo: "fas fa-smoking-ban", name: "No Smoking", id: "no_smoking" },
+    { logo: "fas fa-hot-tub", name: "Jacuzzi", id: "jacuzzi" }, { logo: "fas fa-fish", name: "Fishing", id: "fishing" }
+
     ]
-    
+
     return (
-        <div>
-            <div className="row">
-                <div className="col-6">
-                    <table>
-                        <tr>
-                            <td><p className="text-muted">Bedrooms</p></td>
-                            <td><button>-</button></td>
-                            <td> <p>{}</p></td>
-                            <td><button>+</button></td>
-                        </tr>
-                    </table>
+        <div className='container-fluid'>
+            <div className='d-flex flex-row fontSizeAmenities'>
+                <div className='mr-5 '>
+                    <div className='d-flex flex-row'>
+                        <div> <i class="fa fa-bed text-secondary mr-1" aria-hidden="true "></i> Bedrooms</div>
+                        <div className='ml-5 qtyDiv qtyDivDec mr-3'>-</div>
+                        <div>1</div>
+                        <div className='qtyDiv qtyDivInc ml-3'>+</div>
+                    </div>
                 </div>
-                <div className="col-6">
-                    <table>
-                        <tr>
-                            <td><p className="text-muted">Bathrooms</p></td>
-                            <td><button>-</button></td>
-                            <td> <p>{}</p></td>
-                            <td><button>+</button></td>
-                        </tr>
-                    </table>
+                <div className=' '>
+                    <div className='d-flex flex-row'>
+                        <div> <i class="fa fa-shower" aria-hidden="true"></i> Bathrooms</div>
+                        <div className='ml-5 qtyDiv qtyDivDec mr-3'>-</div>
+                        <div>1</div>
+                        <div className='qtyDiv qtyDivInc ml-3'>+</div>
+                    </div>
                 </div>
             </div>
-            <hr></hr>
-            <div className="row">
-                {
-                    amenites.map(amenite => {
+            <hr />
+
+            <br />
+            <div className='clear-both '></div>
+            <div className='text-secondary mt-3 font-weight-bold '>AMENITIES</div>
+            <div className='d-flex flex-row mt-3'>
+                <div className='row inputTagFilters'>
+                    {amenites.map(item => {
                         return (
-                            <div className="col-4">
-                                <table>
-                                    <tr>
-                                        <td className="p-2" style={{ width: '5px', height: 'auto' }}><input type="checkbox" /></td>
-                                        <td className="p-2"><p>{amenite}</p></td>
-                                    </tr>
-                                </table>
+                            <div key={item.logo} className='col-4 my-2'>
+                                <input className='checkAmenities ' id={item.id} type='checkbox' />
+                                <i className={`px-2 ${item.logo}`}></i>
+                                <span className='fontSizeAmenities'>{item.name}</span>
                             </div>
                         )
-                    })
-                }
+                    })}
+                </div>
             </div>
+            <div className='text-secondary float-right'>
+                close
+            </div>
+            <div >
+                <button className='btn text-white'>Apply</button>
+            </div>
+
         </div>
     )
 }
