8c5971704fbdac16f14ed0a948283baba2b27f35
gangadhar222 <gangadhar.eee@hotmail.com>
2020-07-29 15:43:37 +0530

    google analytics added in routes page

--- tripping/src/Routes/Routes.jsx
+++ tripping/src/Routes/Routes.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import Register from './Authentication/Register'
 import Signin from './Authentication/Signin'
 import VacationRentalsSearch from './VacationRentalsSearch'
 import DetailsCard from '../Components/Card/DetailsCard'
 import TempCard from '../Components/Card/TempCard'
+import ReactGA from 'react-ga';
+
 
 function Routes() {
+    ReactGA.initialize('UA-173994542-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
     return (
         <div>
 
             <Switch>
-            <Route path='/' exact render={()=> <Home />} />
-            <Route path='/vacation-rentals/s/:id' render= {(props)=> <VacationRentalsSearch {...props} />} />
-            <Route path='/register' exact render={()=> <Register />} />
-            <Route path='/signin' exact render={(props)=> <Signin />} />
-            <Route path='/detailscard/:id'  render={(props)=> <DetailsCard {...props} />} />
+                <Route path='/' exact render={() => <Home />} />
+                <Route path='/vacation-rentals/s/:id' render={(props) => <VacationRentalsSearch {...props} />} />
+                <Route path='/register' exact render={() => <Register />} />
+                <Route path='/signin' exact render={(props) => <Signin />} />
+                <Route path='/detailscard/:id' render={(props) => <DetailsCard {...props} />} />
             </Switch>
         </div>
     )
